ff840538ba6e922c3b621cb69a4d8da64b1298ac
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-07 18:44:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,32 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+   data[season].each do |person_hash|
+      if person_hash["status"] == "Winner"
+        return person_hash["name"].split(" ").first
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+    data.keys.each do |key_string|
+      data[key_string].each do |info_hash|
+        if info_hash["occupation"] == occupation
+          return info_hash["name"]
+        end
+      end
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+
+  data.keys.each do |key_string|
+    data[key_string].each do |info_hash|
+      if info_hash["hometown"] == hometown
+        counter =+ 1
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
